f190033d3969ac31cfe541e246d7da1fa2163a83
hsudo <hsudo@hwangseong-ug-ui-MacBookPro.local>
2022-03-02 15:00:43 +0900

    secure update

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+
+const config = require("./config/key")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-// mongodb://3.35.4.211:27017/admin
-mongoose.connect('mongodb://hsudo:Passw0rd1!@3.35.4.211:27017/admin',{
+mongoose.connect(config.mongoURI,{
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!~~ 안녕하세요')
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+    // 회원 가입 할때 필요한 정보들을 client 에서 가져오면
+    // 그것들을 데이터 베이스에 넣는다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
